012e88b4e774a6649c211e6eab5ece4a2eb53187
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-18 20:18:13 +0100

    new2

--- task1.py
+++ task1.py
@@ -2,8 +2,9 @@ from turtle import Turtle, Screen
 
 
 screen = Screen()
-screen.screensize(600, 800)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.title("Pong")
 
 
 screen.exitonclick()
