63bb2d453dbe05d3d58dae2cd9319fb0a2080967
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-18 21:20:03 +0100

    new7

--- main.py
+++ main.py
@@ -12,13 +12,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
-# def go_up():
-#     new_y = paddle.ycor() + 20
-#     paddle.goto(paddle.xcor(), new_y)
 
-# def go_down():
-#     new_y = paddle.ycor() - 20
-#     paddle.goto(paddle.xcor(), new_y)
 
 screen.listen()
 screen.onkey(go_up, "Up")  
