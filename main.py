deaa28bf8bea9bc5888a7e8ef64a5b0242e1d806
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-18 20:22:38 +0100

    new3

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 
+paddle = Turtle()
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.color("white")
 
 screen.exitonclick()
