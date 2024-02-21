144443c691c2534009dd5c69a81b7d37a3cdbdff
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-21 19:52:41 +0100

    new1

--- scoreboard.py
+++ scoreboard.py
@@ -3,7 +3,7 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.color()
+        self.color("white")
         self.penup()
         self.hideturtle()
         self.l_score = 0
